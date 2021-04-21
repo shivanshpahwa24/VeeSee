9af00e3a91c6c411fc08e1a7edbafdea0f725436
Shivansh <shivanshpahwa24@gmail.com>
2021-04-21 19:37:46 +0530

    Context structure done

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import { Typography, AppBar } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-
+import "./App.css";
 import VideoPlayer from "./components/VideoPlayer";
 import Sidebar from "./components/Sidebar";
 import Notifications from "./components/Notifications";
