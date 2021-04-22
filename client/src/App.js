ac41182982bf0035cb84db305283c63e6a262498
Shivansh <shivanshpahwa24@gmail.com>
2021-04-22 19:23:11 +0530

    Preparing for new layout

--- client/src/App.js
+++ client/src/App.js
@@ -6,50 +6,8 @@ import Sidebar from "./components/Sidebar";
 import Notifications from "./components/Notifications";
 import { ContextProvider } from "./Context";
 
-const useStyles = makeStyles((theme) => ({
-  appBar: {
-    borderRadius: 15,
-    margin: "30px 100px",
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-    width: "600px",
-    border: "2px solid black",
-
-    [theme.breakpoints.down("xs")]: {
-      width: "90%",
-    },
-  },
-  image: {
-    marginLeft: "15px",
-  },
-  wrapper: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    width: "100%",
-  },
-}));
-
 const App = () => {
-  const classes = useStyles();
-
-  return (
-    <ContextProvider>
-      <div className={classes.wrapper}>
-        <AppBar className={classes.appBar} position="static" color="inherit">
-          <Typography variant="h2" align="center">
-            Video Chat
-          </Typography>
-        </AppBar>
-        <VideoPlayer />
-        <Sidebar>
-          <Notifications />
-        </Sidebar>
-      </div>
-    </ContextProvider>
-  );
+  return <ContextProvider></ContextProvider>;
 };
 
 export default App;
