e5c950b09455fba8ad175b9eb062307d5c33ffac
Shivansh <shivanshpahwa24@gmail.com>
2021-04-24 04:47:42 +0530

    CSS changes

--- client/src/components/OptionsProvider.js
+++ client/src/components/OptionsProvider.js
@@ -4,6 +4,8 @@ import { CopyToClipboard } from "react-copy-to-clipboard";
 import Snackbar from "@material-ui/core/Snackbar";
 import CallReceivingNotification from "./CallReceivingNotification";
 import { SocketContext } from "../Context";
+import FileCopyIcon from "@material-ui/icons/FileCopy";
+import CallIcon from "@material-ui/icons/Call";
 
 const OptionsProvider = () => {
   const { me, callAccepted, name, setName, callUser, call } = useContext(
@@ -20,50 +22,66 @@ const OptionsProvider = () => {
   });
   const { vertical, horizontal } = direction;
   return (
-    <div className="d-sm-flex justify-content-around align-items-center">
-      <div>
-        <h2 className="grey-text">Call a user</h2>
-        <form className="">
-          <input
-            className="form-control"
-            name="name"
-            placeholder="Enter your name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            id="name"
-            type="text"
-            required
-          />
-          <input
-            className="form-control"
-            type="text"
-            name="id"
-            placeholder="ID to call"
-            value={idToCall}
-            onChange={(e) => setIdToCall(e.target.value)}
-            id="idToCall"
-            required
-          />
-          <button
-            type="button"
-            onClick={() => callUser(idToCall)}
-            className="btn callButton"
-          >
-            Call
-          </button>
-        </form>
+    <div className="d-sm-flex justify-content-between align-items-center">
+      <div className="w-sm-50 w-100 separator py-sm-5">
+        <div className="col-sm-8 mx-auto">
+          <h4 className="options-heading grey-text text-center mb-5">
+            Call a user by specifying their ID
+          </h4>
+          <form className="mx-2">
+            <div className="d-flex my-2">
+              <input
+                className="form-control mr-2"
+                name="name"
+                placeholder="Enter your name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                id="name"
+                type="text"
+                required
+              />
+              <input
+                className="form-control"
+                type="text"
+                name="id"
+                placeholder="ID to call"
+                value={idToCall}
+                onChange={(e) => setIdToCall(e.target.value)}
+                id="idToCall"
+                required
+              />
+            </div>
+            <button
+              type="button"
+              onClick={() => callUser(idToCall)}
+              className="callButton"
+            >
+              <CallIcon fontSize="small" style={{ margin: "0" }} /> Call User
+            </button>
+
+            <hr className="my-5" />
+          </form>
+        </div>
       </div>
-      <div className="d-none d-sm-block headerDivider"></div>
-      <div>
-        <CopyToClipboard
-          text={me}
-          onCopy={() => {
-            /* setCopied(true); */
-            setIdAlertOpen(true);
-          }}
-        >
-          <button>Copy Your ID</button>
-        </CopyToClipboard>
+      {/* <div className="d-none d-sm-block headerDivider p-5"></div> */}
+      <div className="w-sm-50 w-100 text-center mb-5 mb-sm-0">
+        <div className="col-sm-8 mx-auto">
+          <h4 className="options-heading grey-text text-center mb-5">
+            Copy your id and provide it to the person you are trying to
+            communicate with
+          </h4>
+          <CopyToClipboard
+            text={me}
+            onCopy={() => {
+              /* setCopied(true); */
+              setIdAlertOpen(true);
+            }}
+          >
+            <button className="callButton">
+              <FileCopyIcon fontSize="small" /> Copy Your ID
+            </button>
+          </CopyToClipboard>
+        </div>
 
         <Snackbar
           anchorOrigin={{ vertical, horizontal }}
